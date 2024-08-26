b64fb453554b5bd7239e538e71a2ed3f24bf6aef
ChamodKalupahana <chamodkalupahana@gmail.com>
2024-08-26 16:02:54 +0100

    Test commit

---
+++ SwiftThinkingConcurrency/DownloadImagesAsync.swift
@@ -0,0 +1,118 @@
+//
+//  DownloadImagesAsync.swift
+//  SwiftThinkingConcurrency
+//
+//  Created by Chamod on 26/08/2024.
+//
+
+import SwiftUI
+import Combine
+
+class DownloadImageAsyncImageLoader {
+    
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func handleResponse(data : Data?, response : URLResponse?) -> UIImage? {
+        guard
+           let data = data,
+           let image = UIImage(data: data),
+           let response = response as? HTTPURLResponse,
+           response.statusCode >= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error : Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image, error)
+        }
+        .resume()
+    }
+    
+    func downloadWithCombine() -> AnyPublisher<UIImage?, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(handleResponse)
+            .mapError({$0})
+            .eraseToAnyPublisher()
+    }
+    
+    func downloadwithAsync() async throws -> UIImage? {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
+            return handleResponse(data: data, response: response)
+            
+        } catch {
+            throw error
+        }
+    }
+}
+
+class DownloadImagesAsyncViewModel : ObservableObject {
+    
+    @Published var image : UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+    var cancellables = Set<AnyCancellable>()
+    
+    
+    func fetchImage() async {
+        /*
+                loader.downloadWithEscaping { [weak self] image, error in
+        
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+        
+        loader.downloadWithCombine()
+            .sink {_ in
+                
+            } receiveValue: { [weak self] image in
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+            .store(in: &cancellables)
+         */
+        
+        let image = try? await loader.downloadwithAsync()
+        // the main actor is bascially the main thread
+        await MainActor.run {
+            self.image = image
+        }
+    }
+    
+}
+
+struct DownloadImagesAsync: View {
+    
+    @StateObject private var viewModel = DownloadImagesAsyncViewModel()
+    
+    var body: some View {
+        ZStack{
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:250, height: 250)
+            } else {
+                ProgressView()
+            }
+        }
+        //.onAppear(perform:viewModel.fetchImage)
+        // to support concurrency, enter a task and wait for a response
+        .onAppear {
+            Task {
+                await viewModel.fetchImage()
+            }
+        }
+    }
+}
+
+struct DownloadImagesAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImagesAsync()
+    }
+}
+
---
+++ SwiftThinkingConcurrency/SwiftThinkingConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThinkingConcurrencyApp.swift
+//  SwiftThinkingConcurrency
+//
+//  Created by Chamod on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThinkingConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
